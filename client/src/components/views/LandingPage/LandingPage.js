981cfa86c803d05d5d5e5ae724eef87315678aee
BooGeon <qnrjs42@gmail.com>
2020-07-03 10:08:27 +0900

    Grid Card Component

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from "react";
 import { FaCode } from "react-icons/fa";
 import { API_URL, API_KEY, IMAGE_BASE_URL } from '../../Config'
 import MainImage from './Sections/MainImage'
+import GridCards from '../commons/GridCards'
+import { Row } from 'antd'
 
 function LandingPage() {
 
@@ -14,7 +16,9 @@ function LandingPage() {
         fetch(endpoint)
         .then(response => response.json())
         .then(response => {
-            setMovies([response.results])
+            console.log(response.results);
+            
+            setMovies([...response.results])
             setMainMovieImage(response.results[0])
         })
     }, [])
@@ -41,6 +45,21 @@ function LandingPage() {
           </div>
 
           {/* Movie Grid Cards */}
+
+          <Row gutter={[16, 16]} >
+            {Movies && Movies.map((movie, index) => (
+                <React.Fragment key={index}>
+                  <GridCards 
+                    image={movie.poster_path ? `${IMAGE_BASE_URL}w500${movie.poster_path}`: null}
+                    movieId={movie.id}
+                    movieName={movie.original_title}
+                  />
+                </React.Fragment>
+            ))}
+
+          </Row>
+
+          
         </div>
 
         <div style={{ display: "flex", justifyContent: "center" }}>
